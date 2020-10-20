69c0a4b54be8a74323cb019eeed117118d293470
陸玖 南 <h1202019029@hiratagakuen.onmicrosoft.com>
2020-10-20 16:41:47 +0900

    ああああ

--- pr/pr/GameClear.h
+++ pr/pr/GameClear.h
@@ -1,1 +1,24 @@
 #pragma once
+//使用するヘッダー
+#include "GameL/SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：背景
+class GameClera:public 
+{
+public:
+	GameClera:pu();
+	~GameClera:pu();
+
+private:
+
+};
+
+GameClera:pu::GameClera:pu()
+{
+}
+
+GameClera:pu::~GameClera:pu()
+{
+}
