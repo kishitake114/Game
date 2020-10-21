0680681d7cc7a7873d7d932d4f62f72fba67b403
偉 岸本 <h1202019006@hiratagakuen.onmicrosoft.com>
2020-10-21 11:32:27 +0900

    マウスのカーソル・ボタンの出力

--- pr/pr/ObjPlayer.cpp
+++ pr/pr/ObjPlayer.cpp
@@ -1,5 +1,6 @@
 //使用するヘッダー
 #include "GameL/DrawTexture.h"
+#include "GameL/DrawFont.h"
 #include "ObjPlayer.h"
 #include "GameHead.h" 
 #include "GameL/WinInputs.h" 
@@ -9,13 +10,19 @@ using namespace GameL;
 //イニシャライズ
 void CObjPlayer::Init()
 {
-
+	mou_x = 0.0f;
+	mou_y = 0.0f;
+	mou_r = false;
+	mou_l = false;
 }
 
 //アクション
 void CObjPlayer::Action()
 {
-
+	mou_x = (float)Input::GetPosX();
+	mou_y = (float)Input::GetPosY();
+	mou_r = Input::GetMouButtonR();
+	mou_l = Input::GetMouButtonL();
 
 }
 
@@ -23,6 +30,27 @@ void CObjPlayer::Action()
 void CObjPlayer::Draw()
 {
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	//表示：マウスカーソルとボタン
+	wchar_t str[256];
+	swprintf_s(str, L"x=%f,y=%f", mou_x, mou_y);
+	Font::StrDraw(str, 600, 20, 15, c);
+
+	//右クリック
+	if (mou_r == true)
+		Font::StrDraw(L"右=押してる", 600, 40, 20, c);
+	else
+		Font::StrDraw(L"右=押してない", 600, 40, 20, c);
+
+	//左クリック
+	if (mou_l == true)
+		Font::StrDraw(L"左=押してる", 600, 60, 20, c);
+	else
+		Font::StrDraw(L"左=押してない", 600, 60, 20, c);
+
+
+
+	//表示：プレイヤー
 	RECT_F src;
 	RECT_F dst;
 
