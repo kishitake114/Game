1ec049767136907f7fa7499b8810fab89d284a46
祭斗吏 中辻 <h1202019015@hiratagakuen.onmicrosoft.com>
2020-10-22 16:11:41 +0900

    ObjGameOver追加

---
+++ pr/pr/ObjGameOver.cpp
@@ -0,0 +1,27 @@
+//使用するヘッダーファイル
+#include "GameL\DrawFont.h"
+#include "GameL\WinInputs.h"
+#include "GameL\SceneManager.h"
+
+#include "GameHead.h"
+#include "ObjGameOver.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+void CObjGameOver::Init()
+{
+
+}
+
+//アクション
+void CObjGameOver::Action()
+{
+
+}
+
+//ドロー
+void ObjGameOver::Draw()
+{
+
+}
