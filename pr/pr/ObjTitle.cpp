e7b32903fc7c83fd5322cee0738ddd82ff40e610
涼馬 竹之下 <h1202019022@hiratagakuen.onmicrosoft.com>
2020-10-22 14:49:55 +0900

    タイトル

--- pr/pr/ObjTitle.cpp
+++ pr/pr/ObjTitle.cpp
@@ -1,5 +1,8 @@
 //使用するヘッダーファイル
 #include "GameL/DrawTexture.h"
+#include "GameL/DrawFont.h"
+#include "GameL/WinInputs.h"
+#include "GameL/SceneManager.h"
 #include "GameHead.h"
 #include "ObjTitle.h"
 
@@ -9,12 +12,16 @@ using namespace GameL;
 //イニシャライズ
 void CObjTitle::Init()
 {
-
+	m_x = 300;
+	m_y = 200;
 }
 //アクション
 void CObjTitle::Action()
 {
-
+	if (Input::GetVKey(VK_RETURN) == true)
+	{
+		Scene::SetScene(new CSceneMain());
+	}
 }
 //ドロー
 void CObjTitle::Draw()
@@ -22,20 +29,22 @@ void CObjTitle::Draw()
 	//描画カラー情報 R=RED B=Biue A=alpha(透過情報)
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
 
-	RECT_F src; //描画元切り取り位置
-	RECT_F dst; //描画先表示位置
+	Font::StrDraw(L"Enterキーで操作・ルール説明へ", 160, 400, 32, c);
 
-	//切り取り位置の設定
+	//表示：タイトル画面
+	RECT_F src;
+	RECT_F dst;
+
 	src.m_top = 0.0f;
 	src.m_left = 0.0f;
 	src.m_right = 32.0f;
 	src.m_bottom = 32.0f;
-	//表示位置の設定
-	dst.m_top = 0.0f;
-	dst.m_left = 0.0f;
-	dst.m_right = 32.0f;
-	dst.m_bottom = 32.0f;
 
-	//0番目に登録したグラフィックをsrc・dst・cの情報を元に描画
-	Draw::Draw(0, &src, &dst, c, 0.0f);
+	dst.m_top = 0.0f + m_y;
+	dst.m_left = 0.0f + m_x;
+	dst.m_right = 128.0f + m_x;
+	dst.m_bottom = 64.0f + m_y;
+
+	Draw::Draw(1, &src, &dst, c, 0.0f);
+
 }
--- pr/pr/SceneTitle.cpp
+++ pr/pr/SceneTitle.cpp
@@ -5,6 +5,7 @@
 //使用するヘッダー(GameL)
 #include "GameL\DrawTexture.h"
 #include "GameL\SceneObjManager.h"
+#include "GameL/DrawFont.h"
 
 //使用するヘッダー
 #include "SceneTitle.h"
@@ -23,7 +24,9 @@ CSceneTitle::~CSceneTitle()
 //ゲームメイン初期化
 void CSceneTitle::InitScene()
 {
-	Draw::LoadImage(L"Title.png", 0, TEX_SIZE_1024);
+	Font::SetStrTex(L"Enterキーで操作・ルール説明へ");
+
+	Draw::LoadImage(L"Title.png", 1, TEX_SIZE_1024);
 
 	CObjTitle* CObj = new CObjTitle();
 	Objs::InsertObj(CObj, OBJ_TITLE, 1);
